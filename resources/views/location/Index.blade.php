79ee0d692e0cc895d3719db807707d40bfb0b52e
Ersin ÇETİN <ersin.cetin@hotmail.com>
2025-03-18 16:13:15 +0300

    layout create

---
+++ resources/views/location/Index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.layout')
+@section('Title','Location List')
+@section('Meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+@section('Css')
+
+@endsection
+@section('Content')
+
+@endsection
+@section('Javascript')
+
+@endsection
